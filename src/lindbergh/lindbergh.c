2b15dff7bf9c66c36fc176bd0b46349df0f4ba4d
Daniel Martin <dmanlfc@gmail.com>
2025-01-11 21:33:16 +1000

    add sdl2 controller support - best for driving & fighting games

--- src/lindbergh/lindbergh.c
+++ src/lindbergh/lindbergh.c
@@ -11,8 +11,8 @@
 #define LD_LIBRARY_PATH "LD_LIBRARY_PATH"
 #define LD_PRELOAD "LD_PRELOAD"
 #define PRELOAD_FILE_NAME "lindbergh.so"
-#define TEAM "bobbydilley, retrofan, dkeruza-neo, doozer, francesco, rolel, caviar-x"
 #define LINDBERGH_CONFIG_PATH "LINDBERGH_CONFIG_PATH"
+#define TEAM "bobbydilley, retrofan, dkeruza-neo, doozer, francesco, rolel, caviar-x, dmanlfc"
 
 uint32_t elf_crc = 0;
 
